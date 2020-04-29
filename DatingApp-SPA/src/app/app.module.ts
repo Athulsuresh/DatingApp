d39e75f2268ecd2ed5e46ac6596b82f56c70a245
Athul <athulsuresh@ymail.com>
2020-04-29 18:37:07 +0530

    end of sec3

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
-import  { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
    declarations: [
